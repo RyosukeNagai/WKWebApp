d639af6cb2260ead46768eceb18394ef6704d23e
vagrant <vagrant@localhost.localdomain>
2021-01-18 14:37:42 +0000

    firsy

---
+++ Swift6WKWebView1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6WKWebView1
+//
+//  Created by 長井崚介 on 2021/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
